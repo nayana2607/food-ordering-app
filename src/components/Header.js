62f25251b71a806382c84bf80769142f4f1bbce3
Keshav Sharma <keshavs642@gmail.com>
2023-09-21 21:20:20 +0530

    fetching from live data

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,34 @@
+import { useState } from "react";
 import { URLS } from "../utils/constants";
 
 const Header = () => {
-    return (
-      <div className="header">
-        <div className="logo-container">
-          <img
-            className="logo"
-            src={URLS.LOGO_URL}
-          />
-        </div>
-        <div className="nav-items">
-          <ul>
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Contact Us</li>
-            <li>Cart</li>
-          </ul>
-        </div>
-      </div>
+  const handleClick = () => {
+    setButtonLabel(
+      (prevLabel) =>
+        (prevLabel.buttonLabel = buttonLabel === "Login" ? "Logout" : "Login")
     );
   };
+  const [buttonLabel, setButtonLabel] = useState("Login");
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={URLS.LOGO_URL} />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+          <li>
+            <button className="login" onClick={handleClick}>
+              {buttonLabel}
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-  export default Header;
+export default Header;
