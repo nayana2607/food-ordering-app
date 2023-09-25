5a898caf3165c40446990f896b394cf9dee06041
nayanaBhovi <jforme776@gmail.com>
2023-09-25 11:39:24 +0530

    class based components

--- README.md
+++ README.md
@@ -50,3 +50,5 @@ Utility functions
 # Routing in web apps
 - Type 1: Client Side Routing: Internal handling of a route inside of your JS
 - Type 2: Server side Routing: Reuesting new page from server and poviding to user evry time a link is clicked
+
+## Life Cycle method diagram :https://projects.wojtekmaj.pl/react-lifecycle-methods-diagram/
---
+++ src/components/User.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+const User = () => {
+  const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(2);
+  return (
+    <div className="user-card">
+      <h1>{count}</h1>
+      <h2>{count2}</h2>
+      <h2>Name: Nayana</h2>
+      <h3>Location: Vadodara</h3>
+      <h3>Contact:naynuz_99</h3>
+    </div>
+  );
+};
+
+export default User;
